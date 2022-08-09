6831d970a02e4a92be6a06a0fdd72940af203144
Ammar Bin Nasir <majidammar428@gmail.com>
2022-08-09 23:53:17 +0500

    Initial commit

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    test
+    <NuxtPage />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <section>
+    <h1>Home</h1>
+  </section>
+</template>
+
+<script lang="ts" setup>
+
+</script>
